8235ecd08b12903411eadac3bde5032ee7379a01
MrExorcist <112725452+MrExorcist@users.noreply.github.com>
2022-09-08 00:15:49 +0530

    started github

---
+++ uid.js
@@ -0,0 +1,17 @@
+let first_name ="Ayush";
+let last_name ="Shrma";
+let loggedInFromGoogle= true;
+let username= "MrExorcist"
+let password= "qwerty"
+let confirmPass ="qwerty"
+let Age= 20
+
+console.log(password)
+
+console.log(`
+   frst name: ${first_name}
+   last namee:${last_name}
+   weeather logged in from google : ${loggedInFromGoogle}
+   username: ${username}
+
+`)
